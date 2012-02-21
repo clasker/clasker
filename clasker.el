672cbe26544c9d9ff14c48f031bd5f4d0b7354af
Raimon Grau <raimonster@gmail.com>
2012-02-21 02:18:29 +0100

    [core] basic navigation,read of tasks, and creation of tasks

--- clasker.el
+++ clasker.el
@@ -20,12 +20,16 @@
 
 ;;; Commentary:
 
-;; 
+;;
 
 ;;; Code:
 
 (eval-when-compile
   (require 'cl))
+
+(defcustom *clasker-file* "~/.clasker" "File where clasker file tasks are")
+
+(defvar *clasker-tasks* '() "tasks")
 
 (defun clasker-show-tasks (list)
   (let ((n 0))
@@ -39,18 +43,42 @@
   (let ((inhibit-read-only t))
     (erase-buffer)
     (insert (propertize "Clasker\n\n" 'face 'bold))
-    (clasker-show-tasks '("first" "second" "three"))))
+    (clasker-show-tasks (clasker-get-tasks))))
+
+(defun clasker-get-tasks ()
+  (interactive)
+  (when (null *clasker-tasks*)
+    (setf *clasker-tasks* (read-process-file *clasker-file*)))
+  *clasker-tasks*)
+
+(defun read-process-file (file)
+  (when (file-readable-p file)
+    (with-temp-message "Loading tasks..."
+      (with-temp-buffer
+        (insert-file-contents file)
+        (read (current-buffer))))))
 
 (defun clasker-quit ()
   (interactive)
   (kill-buffer "*Clasker*"))
 
+(defun clasker-new-task ()
+  (interactive)
+  (let ((task-desc (read-string "Description:")))
+    (push task-desc *clasker-tasks*))
+  (clasker-revert))
+
 (defvar clasker-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "g") 'revert-buffer)
+    (define-key map (kbd "c") 'clasker-new-task)
     (define-key map (kbd "q") 'clasker-quit)
+    (define-key map (kbd "n") 'next-line)
+    (define-key map (kbd "p") 'previous-line)
     map)
   "docstring")
+
+
 
 (define-derived-mode clasker-mode special-mode "Clasker"
   "docstring"
