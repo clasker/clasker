dc72fe83a85a887c87829bb5a1bc1b4b04a262f0
Raimon Grau <raimonster@gmail.com>
2012-02-25 12:08:01 +0100

    reordered file to load cleanly

--- clasker-edit.el
+++ clasker-edit.el
@@ -1,14 +1,3 @@
-(define-derived-mode clasker-edit-mode text-mode "Clasker Edit"
-  "docstring")
-
-
-(defvar clasker-edit-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c C-c") 'clasker-edit-save-ticket)
-    (define-key map (kbd "C-c C-k") 'clasker-edit-quit)
-    map)
-  "docstring")
-
 (defun clasker-edit-save-ticket ()
   (interactive)
   (setf (cdr (assoc 'description current-ticket))
@@ -21,5 +10,15 @@
   (kill-buffer)
   (delete-window)
   (clasker-revert))
+
+(defvar clasker-edit-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-c") 'clasker-edit-save-ticket)
+    (define-key map (kbd "C-c C-k") 'clasker-edit-quit)
+    map)
+  "docstring")
+
+(define-derived-mode clasker-edit-mode text-mode "Clasker Edit"
+  "docstring")
 
 (provide 'clasker-edit)
