d570949d19af83e7c03a476a7eed2b4c945a3f13
David Vázquez <davazp@gmail.com>
2012-02-26 23:06:03 +0000

    Add header

--- clasker-edit.el
+++ clasker-edit.el
@@ -1,3 +1,32 @@
+;;; clasker-edit.el --- Clasker ticket editing
+
+;; Copyright (C) 2012  Raimon Grau
+;; Copyright (C) 2012  David Vázquez
+
+;; Authors: David Vázquez <davazp@gmail.com>
+;;          Raimon Grau <raimonster@gmail.com>
+
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (defun clasker-edit-save-ticket ()
   (interactive)
   (clasker-ticket-set-property current-ticket 'description (buffer-string))
@@ -22,3 +51,5 @@
   "docstring")
 
 (provide 'clasker-edit)
+
+;;; clasker-edit.el ends here
