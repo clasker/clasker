6e1d8abc0584d899a0cc777be5eb8f2026ae5dd8
David Vázquez <davazp@gmail.com>
2012-03-31 14:00:53 +0100

    Clean tree.

--- clasker.el
+++ clasker.el
@@ -748,6 +748,34 @@ list of tickets to be shown in the current view.")
   (clasker-mode)
   (clasker-revert))
 
+
+
+(defvar current-ticket)
+
+(defun clasker-edit-save-ticket ()
+  (interactive)
+  (clasker-ticket-set-property current-ticket 'description (buffer-string))
+  (clasker-save-ticket current-ticket)
+  (clasker-edit-quit))
+
+(defun clasker-edit-quit ()
+  (interactive)
+  (kill-buffer)
+  (delete-window)
+  (clasker-revert))
+
+(defvar clasker-edit-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-c") 'clasker-edit-save-ticket)
+    (define-key map (kbd "C-c C-k") 'clasker-edit-quit)
+    map)
+  "docstring")
+
+(define-derived-mode clasker-edit-mode text-mode "Clasker Edit"
+  "docstring")
+
+
+
 (provide 'clasker)
 
 ;;; Local variables:
